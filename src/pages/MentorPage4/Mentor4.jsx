7dade05e888a1374399cf5279819cc1d13d3bba6
Shirin <shirinsadeghi@live.com>
2023-11-17 10:36:24 -0800

    begin structuring MentoryPage4

--- src/pages/MentorPage4/Mentor4.jsx
+++ src/pages/MentorPage4/Mentor4.jsx
@@ -1,13 +1,12 @@
 import NavBar from '../../components/NavBar/NavBar';
+import Header from '../../components/Header/Header';
 
 function MentorPage4() {
     return (
         <div>
             <NavBar />
             <div className='mentor'>
-                <h1 className='mentorPage__title'>Here are the best matches for you.</h1>
-                <h2 className='mentorPage__introText'>Based on your preferences, here are the 
-                    top mentors that match. We recommend starting a conversation with one of them.</h2>
+                <Header />
             </div>
         </div>
     )
