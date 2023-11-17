716b740365c748c5e11a54868346f1f0ac542680
David Levett <dntlevett@gmail.comgit config user.namegit config user.emaildntlevett@gmail.com>
2023-11-17 12:47:40 -0800

    add images to matches page and style

--- src/components/MentorCard/MentorCard.jsx
+++ src/components/MentorCard/MentorCard.jsx
@@ -1,10 +1,15 @@
 import "../MentorCard/MentorCard.scss";
+import Ana from "../../assets/ana-nichita.jpg";
+import pedro from "../../assets/pedro-rodriguez.jpg";
+import Victor from "../../assets/victor-quiroga.jpg";
 function MentorCard() {
   return (
     <>
       <div className="mentorCardWrapper">
         <div className="mentorsContainer">
-          <div className="mentorsContainer__image"></div>
+          <div className="mentorsContainer__image">
+            <img className="mentorsContainer__image1" src={pedro} alt="" />
+          </div>
           <div className="mentorsContainer__infoContainer">
             <div className="mentorsContainer__box1">
               <p>Pedro Rodriguez</p>
@@ -19,10 +24,12 @@ function MentorCard() {
           </div>
         </div>
         <div className="mentorsContainer">
-          <div className="mentorsContainer__image"></div>
+          <div className="mentorsContainer__image">
+            <img className="mentorsContainer__image1" src={Ana} alt="" />
+          </div>
           <div className="mentorsContainer__infoContainer">
             <div className="mentorsContainer__box1">
-              <p>Nina Evangelista</p>
+              <p>Ana Nichita</p>
               <button className="mentorsContainer__sendButton">
                 Send Message
               </button>
@@ -34,10 +41,12 @@ function MentorCard() {
           </div>
         </div>
         <div className="mentorsContainer">
-          <div className="mentorsContainer__image"></div>
+          <div className="mentorsContainer__image">
+            <img className="mentorsContainer__image1" src={Victor} alt="" />
+          </div>
           <div className="mentorsContainer__infoContainer">
             <div className="mentorsContainer__box1">
-              <p>Max Dela Peña</p>
+              <p>Victor Quiroga</p>
               <button className="mentorsContainer__sendButton">
                 Send Message
               </button>
