c462714280292e440ff86be4e1296e5d24aacd5e
Shirin <shirinsadeghi@live.com>
2023-11-16 23:59:08 -0800

    create NavBarand start Mentor Page3.jsx

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import './NavBar.scss'
+
+function NavBar() {
+    return (
+        <nav className="nav">
+            <h1>Hello World</h1>
+        </nav>
+    )
+}
+
+export default NavBar
--- src/pages/MentorPage3/Mentor3.jsx
+++ src/pages/MentorPage3/Mentor3.jsx
@@ -1,4 +1,20 @@
+import NavBar from '../../components/NavBar/NavBar';
+
 function MentorPage3() {
+    return (
+        <div>
+             <NavBar/>
+             <div className='message'>
+                <span className='message__box'>Step 3</span>
+                <h2 className='message__title'>Messae to your mentors</h2>
+                <form className='message__form'>
+                    <textarea name='text'>Anything you'd like to ask?</textarea>
+                </form>
+            </div>
+            
+        </div>
+       
+    )
 
 }
 
