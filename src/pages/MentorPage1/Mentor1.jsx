c979f07be327f869bdf25daf49be345a353ed568
David Levett <dntlevett@gmail.comgit config user.namegit config user.emaildntlevett@gmail.com>
2023-11-17 00:24:55 -0800

    start and nearly complete mentor match 1

--- src/pages/MentorPage1/Mentor1.jsx
+++ src/pages/MentorPage1/Mentor1.jsx
@@ -1,5 +1,34 @@
+import "../MentorPage1/mentor1.scss";
 function MentorPage1() {
-
+  return (
+    <div className="mentorPage1Container">
+      {/* the wording below could use some grammar adjustments? */}
+      <p className="mentorPage1Container__introText">
+        Get matches in a few easy steps. We'll help ensure the mentors are
+        relevant to you, and having upcoming availabilities
+      </p>
+      <p className="mentorPage1Container__GoalText">
+        What's your top goal right now?
+      </p>
+      <div>
+        <p className="mentorPage1Container__step1Button">Step 1</p>
+      </div>
+      <div className="mentorPage1Container__goalsContainer">
+        <button className="mentorPage1Container__goalsContainer--option">
+          Improve my storefront
+        </button>
+        <button className="mentorPage1Container__goalsContainer--option">
+          Learn about logistics
+        </button>
+        <button className="mentorPage1Container__goalsContainer--option">
+          Business management
+        </button>
+        <button className="mentorPage1Container__goalsContainer--option">
+          Marketing advice
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default MentorPage1
+export default MentorPage1;
