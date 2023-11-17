42948ed044e4db57c56f20b7029ea60af57221ca
David Levett <dntlevett@gmail.comgit config user.namegit config user.emaildntlevett@gmail.com>
2023-11-17 07:58:45 -0800

    start and finish html layout for mentor match 2 page

--- src/pages/MentorPage2/Mentor2.jsx
+++ src/pages/MentorPage2/Mentor2.jsx
@@ -1,5 +1,37 @@
 function MentorPage2() {
-
+  return (
+    <div className="mentorPage2Container">
+      <p>Step 2</p>
+      <p>Which areas would you like to get mentored in?</p>
+      <div>
+        <button>Accounting</button>
+        <button>Legal</button>
+        <button>Marketing</button>
+      </div>
+      <div>
+        <button>Shipping</button>
+        <button>Development</button>
+        <button>Logistics</button>
+      </div>
+      <p>Any specific tools you'd like help with?</p>
+      <div>
+        <button>Themes</button>
+        <button>Shipping</button>
+        <button>Marketing</button>
+      </div>
+      <div>
+        <button>Theme Design</button>
+        <button>Development</button>
+        <button>Logistics</button>
+      </div>
+      <p>What Language are you comfortable with?</p>
+      <div>
+        <button>English</button>
+        <button>Spanish</button>
+        <button>French</button>
+      </div>
+    </div>
+  );
 }
 
-export default MentorPage2
+export default MentorPage2;
