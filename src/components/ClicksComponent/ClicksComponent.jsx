7b19d81af7f547e1ecf3185eac3517e73876860e
David Levett <dntlevett@gmail.comgit config user.namegit config user.emaildntlevett@gmail.com>
2023-11-17 16:24:45 -0800

    fix name

--- src/components/ClicksComponent/ClicksComponent.jsx
+++ src/components/ClicksComponent/ClicksComponent.jsx
@@ -1,69 +1,133 @@
 import { useState } from "react";
-import './clickscomponent.scss';
+import "./clickscomponent.scss";
 
 function ClicksComponent() {
+  const [showClicked, setShowClicked] = useState({});
 
-const [showClicked, setShowClicked] = useState({});
-
-const handleClick = (buttonName) => {
-    setShowClicked(prevState => ({
-        ...prevState, [buttonName]: !prevState[buttonName]
+  const handleClick = (buttonName) => {
+    setShowClicked((prevState) => ({
+      ...prevState,
+      [buttonName]: !prevState[buttonName],
     }));
-}
+  };
 
-    return (
-        <>
-        <div>
-        <p className="mentor__sub-title">Which areas would you like to get mentored in?</p>
-            <div className="click__container">
-                <button className={showClicked['Accounting'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Accounting')}>Accounting</button>
-                <button className={showClicked['Legal']  ? "active" : "not-active"} 
-                    onClick={() => handleClick('Legal')}>Legal</button>
-                <button className={showClicked['Marketing'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Marketing')}>Marketing</button>
-            </div>
-            <div className="click__container">
-                <button className={showClicked['Shipping'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Shipping')}>Shipping</button>
-                <button className={showClicked['Development'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Development')}>Development</button>
-                <button className={showClicked['Logistics'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Logistics')}>Logistics</button>
-            </div> 
-        </div>
-        <p className="mentor__sub-title">Any specific tools you'd like help with?</p>
-        <div>
-            <div className="click__container">
-                <button className={showClicked['Themes'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Themes')}>Themes</button>
-                <button className={showClicked['Shipping2'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Shipping2')}>Shipping</button>
-                <button className={showClicked['Marketing2'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Marketing2')}>Marketing</button>
-            </div>
-            <div className="click__container">
-                <button className={showClicked['Theme Design'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Theme Design')}>Theme Design</button>
-                <button className={showClicked['Development2'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Development2')}>Development</button>
-                <button className={showClicked['Logistics2'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Logistics2')}>Logistics</button>
-            </div>
-        </div>
-        <p className="mentor__sub-title">What Language are you comfortable with?</p>
+  return (
+    <>
+      <div>
+        <p className="mentor__sub-title">
+          Which areas would you like to get mentored in?
+        </p>
         <div className="click__container">
-            <button className={showClicked['English'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('English')}>English</button>
-            <button className={showClicked['Spanish'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('Spanish')}>Spanish</button>
-            <button className={showClicked['French'] ? "active" : "not-active"} 
-                    onClick={() => handleClick('French')}>French</button>
+          <button
+            className={showClicked["Accounting"] ? "active" : "not-active"}
+            onClick={() => handleClick("Accounting")}
+          >
+            Accounting
+          </button>
+          <button
+            className={showClicked["Legal"] ? "active" : "not-active"}
+            onClick={() => handleClick("Legal")}
+          >
+            Legal
+          </button>
+          <button
+            className={showClicked["Marketing"] ? "active" : "not-active"}
+            onClick={() => handleClick("Marketing")}
+          >
+            Marketing
+          </button>
         </div>
-        </>
-    )
-    
-       
+        <div className="click__container">
+          <button
+            className={showClicked["Shipping"] ? "active" : "not-active"}
+            onClick={() => handleClick("Shipping")}
+          >
+            Logistics
+          </button>
+          <button
+            className={showClicked["Development"] ? "active" : "not-active"}
+            onClick={() => handleClick("Development")}
+          >
+            Development
+          </button>
+          <button
+            className={showClicked["Logistics"] ? "active" : "not-active"}
+            onClick={() => handleClick("Logistics")}
+          >
+            Influence
+          </button>
+        </div>
+      </div>
+      <p className="mentor__sub-title">
+        Any specific tools you'd like help with?
+      </p>
+      <div>
+        <div className="click__container">
+          <button
+            className={showClicked["Themes"] ? "active" : "not-active"}
+            onClick={() => handleClick("Themes")}
+          >
+            Shipping
+          </button>
+          <button
+            className={showClicked["Shipping2"] ? "active" : "not-active"}
+            onClick={() => handleClick("Shipping2")}
+          >
+            Shopify Plus
+          </button>
+          <button
+            className={showClicked["Marketing2"] ? "active" : "not-active"}
+            onClick={() => handleClick("Marketing2")}
+          >
+            Marketing Tools
+          </button>
+        </div>
+        <div className="click__container">
+          <button
+            className={showClicked["Theme Design"] ? "active" : "not-active"}
+            onClick={() => handleClick("Theme Design")}
+          >
+            Theme Design
+          </button>
+          <button
+            className={showClicked["Development2"] ? "active" : "not-active"}
+            onClick={() => handleClick("Development2")}
+          >
+            3rd Party Apps
+          </button>
+          <button
+            className={showClicked["Logistics2"] ? "active" : "not-active"}
+            onClick={() => handleClick("Logistics2")}
+          >
+            Logistics
+          </button>
+        </div>
+      </div>
+      <p className="mentor__sub-title">
+        What Language are you comfortable with?
+      </p>
+      <div className="click__container">
+        <button
+          className={showClicked["English"] ? "active" : "not-active"}
+          onClick={() => handleClick("English")}
+        >
+          English
+        </button>
+        <button
+          className={showClicked["Spanish"] ? "active" : "not-active"}
+          onClick={() => handleClick("Spanish")}
+        >
+          Spanish
+        </button>
+        <button
+          className={showClicked["French"] ? "active" : "not-active"}
+          onClick={() => handleClick("French")}
+        >
+          More
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default ClicksComponent
+export default ClicksComponent;
--- src/components/MessageForm/MessageForm.jsx
+++ src/components/MessageForm/MessageForm.jsx
@@ -1,28 +1,40 @@
-import './MessageForm.scss';
+import "./MessageForm.scss";
 
 function MessageForm() {
-    return(
-        <div className='message'>
-            <div className='step__box'>
-                <span >Step 3</span>
-            </div>
-            <h2 className='message__title'>Message to your mentors</h2>
-            <form className='message__form'>
-                <textarea className="message__text" name='text' placeholder='Anything you would like to ask?'></textarea>
-            </form>
-            <h2 className='message__title'>Send a message</h2>
-        
-            <div className='message__container'>
-                <p className='message__default-comments'>Sample greeting, intro 1</p>
-            </div>
-            <div className='message__container'>
-                <p className='message__default-comments'>Sample greeting, intro 2</p>
-            </div>
-            <div className='message__container'>
-                <p className='message__default-comments'>Sample greeting, intro 3</p>
-            </div>
-        </div> 
-    )
+  return (
+    <div className="message">
+      <div className="step__box">
+        <span>Step 3</span>
+      </div>
+      <h2 className="message__title">Message to your mentors</h2>
+      <form className="message__form">
+        <textarea
+          className="message__text"
+          name="text"
+          placeholder="Anything you would like to ask?"
+        ></textarea>
+      </form>
+      <h2 className="message__title">Send a message</h2>
+
+      <div className="message__container message__container--alt">
+        <p className="message__default-comments message__default-commentsAlt">
+          I’m looking for guidance on navigating international shipping for my
+          coffee business.
+        </p>
+      </div>
+      <div className="message__container">
+        <p className="message__default-comments">
+          Looking for tips on marketing Colombian coffee internationally
+        </p>
+      </div>
+      <div className="message__container">
+        <p className="message__default-comments">
+          Can you help me find available shipping and marketing tools through
+          Shopify platform
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default MessageForm
+export default MessageForm;
